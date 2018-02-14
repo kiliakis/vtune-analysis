c83e6b8737ce41bc1805b7bba1e832983b8d2455
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2018-02-14 14:34:03 +0100

    renamed scripts, improved report script

---
+++ report.py
@@ -0,0 +1,38 @@
+import sys
+import subprocess
+import argparse
+
+# usage: python report.py -r [summary | hw-events] -i [indir] -o [outfile]
+
+parser = argparse.ArgumentParser(description='Generate a report from the input raw data.',
+                                 usage='python report.py -r [summary | hw-events] -i [indir] -o [outfile]')
+
+parser.add_argument('-r', '--report', type=str, default='hw-events',
+                    help='The report type. (summary or hw-events)'
+                    ' Default: hw-events')
+
+parser.add_argument('-o', '--outfile', type=str, default=None,
+                    help='The file to save the report.'
+                    ' Default: None (print to the stdout)')
+
+parser.add_argument('-i', '--indir', type=str, default=None,
+                    help='The directory containing the collected data.'
+                    ' Default: None (print to the stdout)')
+
+
+amplxe_args = [
+    'amplxe-cl', '-report',
+    '', '-csv-delimiter=tab',
+    '-format', 'csv'
+]
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    amplxe_args[2] = args.report
+    if args.report == 'hw-events':
+        amplxe_args += ['-group-by', 'task']
+    if args.outfile:
+        amplxe_args += ['-report-output', args.outfile]
+    if args.infile:
+        amplxe_args += ['-result-dir', args.indir]
+    subprocess.call(amplxe_args)
